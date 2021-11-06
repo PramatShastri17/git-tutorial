3cfe495bca8361abea2673b7a2cd69252a4e3b9a
PramatShastri17 <85291394+PramatShastri17@users.noreply.github.com>
2021-11-06 15:08:26 +0530

    Added cosine plot

--- test.py
+++ test.py
@@ -3,9 +3,12 @@ print('Hello World!!')
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-5, 5, 100)
-y = np.sin(x)
+x = np.linspace(-10, 10, 100)
+y1 = np.sin(x)
+y2 = np.cos(x)
 
-plt.plot(x, y, 'b-')
+plt.plot(x, y1, 'b-',label='Sine')
+plt.plot(x, y2, 'r-',label='Cosine')
 plt.grid(linestyle='--')
+plt.legend()
 plt.show()
